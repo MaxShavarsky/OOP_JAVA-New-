e457024cb5ae1133bc9beebb9bd57f0fc30b126d
MaxShavarsky <37290488+MaxShavarsky@users.noreply.github.com>
2018-03-18 22:50:33 +0200

    Update Laba_1_week_3_task_3.java

--- Labs/Lab_2/Laba_1_week_3_task_3.java
+++ Labs/Lab_2/Laba_1_week_3_task_3.java
@@ -1,1 +1,11 @@
+public class Fibonacci
+{
+	public static long getNumber(int position){
+    if (position <  1) return -1;
+    if (position == 1) return 1;
+    if (position == 2) return 1;
+    return getNumber(position - 1) + getNumber(position - 2);
 
+	}
+}
+      
