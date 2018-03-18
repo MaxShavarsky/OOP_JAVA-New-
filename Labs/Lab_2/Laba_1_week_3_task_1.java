33cfbf4c0173d34fdf0c40d679d0e6fdedac97e8
MaxShavarsky <37290488+MaxShavarsky@users.noreply.github.com>
2018-03-18 22:58:55 +0200

    Update Laba_1_week_3_task_1.java

--- Labs/Lab_2/Laba_1_week_3_task_1.java
+++ Labs/Lab_2/Laba_1_week_3_task_1.java
@@ -1,81 +1,82 @@
 package com.tasks3.linkedlist;
 
 public class LinkedList {
- private Node _head;
-    private Node _tail;
-    private int count=0;
-  public LinkedList() {
- 
-  }
+	private Node golova;
+	private Node hvist;
+	private int count = 0;
+	public LinkedList() {
 
-   public void add(Integer data){
-        Node Spysok=new Node();
-        Spysok.setData(data);
-        if(_head==null){
-            _head=Spysok;
-            _tail=Spysok;
-        }
-        else{
-            _tail.setNext(Spysok);
-            _tail=Spysok;
-        }
-        count++;
-    }
-    public Integer get(int index) {
-        Node Spysok = _head;
-        if (index > count) {
-            return null;
-        } else {
-            for (int i = 0; i <= index; i++) {
-                if (i == index) {
-                    return Spysok.getData();
-                }
-                Spysok = Spysok.getNext();
+	}
 
-            }
-        }
-        return null;
-    }
-    public int size(){
-        return count;
-    }
-    public boolean delete(int index){
-        Node useless=null;
-        Node useless2=_head;
-        if(index>count){
-            return false;
-        }
-        else if(index==0){
-           _head=_head.getNext();
-           if(_head==null){
-               _tail=null;
-           }
-           count--;
-           return true;
-        }
-       else if(index==count-1){
-            for(int i=0;i<=index;i++){
-                if(i==index){
-                    _tail=useless;
-                    _tail.setNext(null);
-                    count--;
-                    return true;
-                }
-                useless=useless2;
-                useless2=useless2.getNext();
-            }
-        }
-        else{
-            for(int i=0;i<=index;i++){
-                if(i==index){
-                    useless.setNext(useless2.getNext());
-                    count--;
-                    return true;
-                }
-                useless=useless2;
-                useless2=useless2.getNext();
-            }
-        }
-        return false;
-    }
-    }
+	public void add(Integer data) {
+		Node Spysok = new Node();
+		Spysok.setData(data);
+		if (golova == null) {
+			golova = Spysok;
+			hvist = Spysok;
+		}
+		else {
+			hvist.setNext(Spysok);
+			hvist = Spysok;
+		}
+		count++;
+	}
+	public Integer get(int index) {
+		Node Spysok = golova;
+		if (index > count) {
+			return null;
+		}
+		else {
+			for (int i = 0; i <= index; i++) {
+				if (i == index) {
+					return Spysok.getData();
+				}
+				Spysok = Spysok.getNext();
+
+			}
+		}
+		return null;
+	}
+	public int size() {
+		return count;
+	}
+	public boolean delete(int index) {
+		Node mysor = null;
+		Node mysor2 = golova;
+		if (index>count) {
+			return false;
+		}
+		else if (index == 0) {
+			golova = golova.getNext();
+			if (golova == null) {
+				hvist = null;
+			}
+			count--;
+			return true;
+		}
+		else if (index == count - 1) {
+			for (int i = 0; i <= index; i++) {
+				if (i == index) {
+					hvist = mysor;
+					hvist.setNext(null);
+					count--;
+					return true;
+				}
+				mysor = mysor2;
+				mysor2 = mysor2.getNext();
+			}
+		}
+		else {
+			for (int i = 0; i <= index; i++) {
+				if (i == index) {
+					mysor.setNext(mysor2.getNext());
+					count--;
+					return true;
+				}
+				mysor = mysor2;
+				mysor2 = mysor2.getNext();
+			}
+		}
+		return false;
+	}
+}
