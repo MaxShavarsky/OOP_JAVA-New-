f099724abff70b0c434682fc441bf45311ac8c86
MaxShavarsky <37290488+MaxShavarsky@users.noreply.github.com>
2018-03-29 15:07:56 +0300

    Update Lab_7.java

--- Labs/Lab_7/Lab_7.java
+++ Labs/Lab_7/Lab_7.java
@@ -4,46 +4,63 @@ import java.util.Scanner;
 public class lab {
 
     public static void main(String[] args) {
-        String text,temp="",first ="",second="";
-        List<Integer> limit = new LinkedList<>();
-        List<String> words = new LinkedList<>();
-        Scanner sc = new Scanner(System.in);
-        text = sc.nextLine();
-        for (int i = 0; i < text.length(); i++)
-        {
-            if (text.charAt(i) == ' '|| text.charAt(i) == '.' || text.charAt(i) == ',' || text.charAt(i) == '!' || text.charAt(i) == '?' || text.charAt(i) == ':')
-            {
-                limit.add(i);
+        Boolean debug = false;
+        if (args.length == 1) {
+            if (args[0].equalsIgnoreCase( "-help") || args[0].equalsIgnoreCase("-h")) {
+                System.out.println("Max Shavarsky , KN-110 , Lab 8 ,\nTask 8.\n\n" +
+                        "Enter text. In the text, find all pairs of words, one of which is the turn (the word of the opposite) of another (for example: abc \"-\" cba \",\" def \"-\" fed \"). The result is displayed in the form of a table.\n Arguments:" + args[0]);
+                return;
+            } else if (args[0].equalsIgnoreCase( "-debug")|| args[0].equalsIgnoreCase( "-d")) {
+                debug = true;
             }
-        }
-        for (int i = 0; i < limit.size()-1; i++)
-        {
-            for (int j = limit.get(i)+1; j < limit.get(i+1); j++)
-            {
-                temp = temp + text.charAt(j);
-            }
-            words.add(temp);
-            temp = "";
-        }
 
-        for (int i = 0; i < words.size(); i++)
-        {
-            first = words.get(i);
-            for (int j = first.length()-1; j >= 0; j--) {
-                second = second + first.charAt(j);
-            }
-            for (int f = 0; f < words.size(); f++)
-            {
-                if (second.equals(words.get(f)))
-                {
-                    System.out.println(first + "<-->"+second);
-                    second = "";
-                    break;
+                String text, temp = "", first = "", second = "";
+                List<Integer> limit = new LinkedList<>();
+                List<String> words = new LinkedList<>();
+                Scanner sc = new Scanner(System.in);
+                text = sc.nextLine();
+                for (int i = 0; i < text.length(); i++) {
+                    if (text.charAt(i) == ' ' || text.charAt(i) == '.' || text.charAt(i) == ',' || text.charAt(i) == '!' || text.charAt(i) == '?' || text.charAt(i) == ':') {
+                        limit.add(i);
+                    }
                 }
-            }
-            second = "";
-        }
+                if (debug == true) {
+                    for (int i = 0; i < limit.size(); i++) {
+                        System.out.print(limit.get(i));
+                        System.out.print(" ");
+                    }
+                    System.out.print("\n");
+                }
+                for (int i = 0; i < limit.size() - 1; i++) {
+                    for (int j = limit.get(i) + 1; j < limit.get(i + 1); j++) {
+                        temp = temp + text.charAt(j);
+                    }
+                    words.add(temp);
+                    temp = "";
+                }
+                if (debug == true) {
+                    for (int i = 0; i < words.size(); i++) {
+                        System.out.print(words.get(i));
+                        System.out.print(" ");
+                    }
+                    System.out.print("\n");
+                }
+                for (int i = 0; i < words.size(); i++) {
+                    first = words.get(i);
+                    for (int j = first.length() - 1; j >= 0; j--) {
+                        second = second + first.charAt(j);
+                    }
+                    for (int f = 0; f < words.size(); f++) {
+                        if (second.equals(words.get(f))) {
+                            System.out.println(first + "<-->" + second);
+                            second = "";
+                            break;
+                        }
+                    }
+                    second = "";
+                }
 
+            
+        }
     }
 }
-
